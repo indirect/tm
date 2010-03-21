0b1c6c64a793aca56b8b0758157c6070dd7f6e04
Andre Arko <andre@arko.net>
2010-03-21 16:23:53 -0700

    tm doesn't need to tab-complete files

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Just add tab-completion to `tm`
-complete -f -W '$(ls ~/.tmproj | sed s/.tmproj//)' tm
+complete -W '$(ls ~/.tmproj | sed s/.tmproj//)' tm
 
 # Also wrap `mate` with `tm` and add tab-completion
 alias mate='tm'
